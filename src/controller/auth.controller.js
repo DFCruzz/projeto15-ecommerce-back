a0306b0dc0732ff1d145ee385d942cb95997f535
lucas-lima-dev <lucas.sdslima@gmail.com>
2023-01-26 23:15:51 -0300

    feat/sign-in

---
+++ src/controller/auth.controller.js
@@ -0,0 +1,38 @@
+import bcrypt from "bcrypt";
+import { v4 as uuidV4 } from "uuid";
+import { usersCollection, sessionsCollection } from "../config/database.js";
+
+export async function signUp(req, res) {
+  const newUser = res.locals.user;
+
+  const hashPassword = bcrypt.hashSync(newUser.password, 10);
+
+  try {
+    await usersCollection.insertOne({ ...newUser, password: hashPassword });
+    return res.status(201).send("Usuário cadastrado com sucesso");
+  } catch (error) {
+    return res.status(500).send("Deu algo errado no servidor");
+  }
+}
+
+export async function signIn(req, res) {
+  
+  const userRegistered = res.locals.userRegistered;
+  const tokenExists = res.local.tokenExists;
+  const token = uuidV4();
+
+  try {
+    if (tokenExists) {
+      await sessionsCollection.updateOne(
+        { idUser: userRegistered._id },
+        { $set: { token } }
+      );
+      return res.status(200).send({ token });
+    } else {
+      await sessionsCollection.insertOne({ idUser: userRegistered._id, token });
+      return res.status(200).send({ token });
+    }
+  } catch (error) {
+    return res.status(500).send("Deu algo errado no servidor");
+  }
+}
