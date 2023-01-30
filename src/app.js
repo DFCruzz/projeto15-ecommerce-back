86639630b150ef2c550098fabd2f8baecd9d2573
DFCruzz <diegofcruzz@gmail.com>
2023-01-29 22:58:11 -0300

    funcoes produtos e database externo adicionados

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 import express,{json} from "express";
 import cors from "cors";
 import authRouter from "./routes/AuthRoutes.js";
+import ProductRouter from "./routes/ProductRouter.js";
 
 
 const PORT = process.env.PORT || 5008;
 const app = express();
 
 
-app.use([cors(),json()],authRouter);
+app.use([cors(),json()],[authRouter, ProductRouter]);
 
 
 app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
